9aa09719211d1975ff94da2b018cf9ca609442d0
v_dmitrov <v.dmitrov@smartmerch.it>
2025-06-03 15:50:13 +0300

    add jwt auth

--- service/user.py
+++ service/user.py
@@ -1,21 +1,17 @@
-import string
 from dataclasses import dataclass
-from os import access
-from random import choice
 
 from repository import UserRepository
 from schema import UserLoginSchema
+from service.auth import AuthService
 
 
 @dataclass
 class UserService:
     user_repository: UserRepository
+    auth_service: AuthService
 
     def create_user(self, user_name: str, password: str) -> UserLoginSchema:
-        access_token = self._generate_access_token()
-        user = self.user_repository.create_user(user_name=user_name, password=password, access_token=access_token)
-        return UserLoginSchema(user_id=user.id, access_token=user.access_token)
+        user = self.user_repository.create_user(user_name=user_name, password=password)
+        access_token = self.auth_service.generate_access_token(user_id = user.id)
+        return UserLoginSchema(user_id=user.id, access_token=access_token)
 
-    @staticmethod
-    def _generate_access_token() -> str:
-        return ''.join(choice(string.ascii_uppercase + string.digits) for _ in range(10))
